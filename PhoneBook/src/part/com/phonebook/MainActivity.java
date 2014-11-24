7a4faed7192815b8a2281bae3186a5c8be2899c4
skoja <jasmin.jahic@gmail.com>
2014-11-24 21:15:46 +0100

    Hello phone book.

---
+++ PhoneBook/src/part/com/phonebook/MainActivity.java
@@ -0,0 +1,49 @@
+package part.com.phonebook;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        /*Intent intent = new Intent();
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);*/
+    	TextView textView = (TextView) findViewById(R.id.textView1);
+    	textView.setText("Hello Phone book!");
+    }
+}
